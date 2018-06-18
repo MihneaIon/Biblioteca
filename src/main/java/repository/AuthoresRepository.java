ebbabe6594bde9b1712fa4381565aea2ce0069bb
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-06-18 11:17:58 +0300

    Add files via upload

---
+++ src/main/java/repository/AuthoresRepository.java
@@ -0,0 +1,9 @@
+package com.siemens.Library.library.repository;
+
+import com.siemens.Library.library.Authores;
+import org.springframework.data.repository.CrudRepository;
+
+public interface AuthoresRepository extends CrudRepository<Authores,Long>
+{
+
+}

e96a27f9e8001bfc88c0c6f14ade172f3b1937e0
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-06-18 11:31:20 +0300

    Add files via upload

---
+++ src/main/java/repository/GenreRepository.java
@@ -0,0 +1,10 @@
+package com.siemens.Library.library.repository;
+
+import com.siemens.Library.library.Genre;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GenreRepository extends CrudRepository<Genre,Long> {
+
+
+
+}

061a48c142c9f172d97be90c75254620b34bb401
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-06-26 14:27:39 +0300

    Update GenreController.java

--- src/main/java/controlers/GenreController.java
+++ src/main/java/controlers/GenreController.java
@@ -3,10 +3,10 @@ package com.siemens.Library.library.controler;
 import com.siemens.Library.library.Genre;
 import com.siemens.Library.library.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class GenreController {
@@ -14,11 +14,50 @@ public class GenreController {
     @Autowired
     GenreRepository myGenreRepository;
 
-    @RequestMapping(value = "/genres",method = RequestMethod.POST)
-    public void addBook(@RequestParam(value = "name",defaultValue = "Darama")String name)
+//    @RequestMapping(value = "/genre",method = RequestMethod.POST)
+//    public Genre addBook(@RequestParam(value = "name",defaultValue = "Darama")String name)
+//    {
+//        Genre myGenre=new Genre();
+//        myGenreRepository.save(myGenre);
+//        return  myGenre;
+//    }
+
+    @RequestMapping(value = "/genre",method = RequestMethod.POST)
+    public void addGen(@RequestBody Genre myGenre)
     {
-        Genre myGenre=new Genre();
-        myGenreREepository.save(myGenre);
+        myGenreRepository.save(myGenre);
+    }
+
+    @RequestMapping(value = "/genre",method = RequestMethod.GET)
+    public List<Genre> getGenre()
+    {
+        return (List<Genre>) myGenreRepository.findAll();
+    }
+
+    @RequestMapping(value = "/genre",method = RequestMethod.PUT)
+    public Genre updateAuthor(@PathVariable(value = "genre_id")Long genreId,
+                                 @RequestBody Genre myGenre)
+    {
+        try{
+
+            Genre auxGenre=myGenreRepository.findById(genreId).get();
+            auxGenre.setName(myGenre.getName());
+            Genre updateGenre=myGenreRepository.save(auxGenre);
+            return updateGenre;
+
+        }catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @RequestMapping(value = "/genre/{genre_id}",method = RequestMethod.DELETE)
+    public ResponseEntity<?> deleteGenre(@PathVariable(value = "genre_id")Long genreId)
+    {
+        Genre myGenre=myGenreRepository.findById(genreId).get();
+        myGenreRepository.delete(myGenre);
+        return  ResponseEntity.ok().build();
     }
 
 
